bcf663cb848b493830b24b20bc728e57b471c4d5
Richard Azia <richard@main-vision.com>
2022-11-26 12:35:50 +0100

    corrected two typos and added policies

--- resources/views/chirps/index.blade.php
+++ resources/views/chirps/index.blade.php
@@ -23,7 +23,7 @@
                                 <span class="text-gray-800">{{ $chirp->user->name }}</span>
                                 <small class="ml-2 text-sm text-gray-600">{{ $chirp->created_at->format('j M Y, g:i a') }}</small>
                                 @unless ($chirp->created_at->eq($chirp->updated_at))
-                                  <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
+                                    <small class="text-sm text-gray-600"> &middot; {{ __('edited') }}</small>
                                 @endunless
                             </div>
                             @if ($chirp->user->is(auth()-> user()))
@@ -41,6 +41,7 @@
                                 </x-dropdown-link>
                               </x-slot>
                             </x-dropdown>
+                          @endif
                         </div>
                         <p class="mt-4 text-lg text-gray-900">{{ $chirp->message }}</p>
                     </div>
