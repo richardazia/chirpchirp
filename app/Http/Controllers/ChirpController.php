2871b4bf1ea82e8110ad087ea196a4c21f9f19e9
Richard Azia <richard@main-vision.com>
2022-11-26 11:43:30 +0100

    store and create relationship

--- app/Http/Controllers/ChirpController.php
+++ app/Http/Controllers/ChirpController.php
@@ -36,7 +36,13 @@ class ChirpController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $validate = $request->validate([
+          'message' => 'required|string|max:255',
+        ]);
+
+        $request->user()->chirps()->create($validated);
+
+        return redirect(route('chirps.index'));
     }
 
     /**
